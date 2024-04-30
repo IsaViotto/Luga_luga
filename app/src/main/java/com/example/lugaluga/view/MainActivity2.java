021727d28999f531b65c70619fed085590738553
Isadora Viotto <isavbiten@outlook.com>
2024-04-30 16:49:51 -0300

    Aula dia 30/04

--- app/src/main/java/com/example/lugaluga/view/MainActivity2.java
+++ app/src/main/java/com/example/lugaluga/view/MainActivity2.java
@@ -12,43 +12,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lugaluga.R;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private ListView listCidade;
-
-    private String[] cidades = {
-            "São Carlos", "Araraquara", "Ibaté", "Ribeirão Bonito",
-            "Dourado", "Descalvado", "Porto Ferreira", "Tambaú",
-            "Santa Rita do Passa Quatro", "Pirassununga", "Ribeirão Preto",
-            "Jaboticabal", "Franca", "São Paulo", "Jaú", "Rio Claro", "Brotas",
-            "Bauru", "Campinas"
-    };
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        listCidade = findViewById(R.id.lista);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                cidades
-        );
-
-        listCidade.setAdapter(adapter);
-
-        listCidade.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String itemSelecionado = listCidade.getItemAtPosition(position).toString();
-                Toast.makeText(getApplicationContext(), itemSelecionado, Toast.LENGTH_LONG).show();
-            }
-        });
+        recyclerView.findViewById(R.id.listaProdutos);
     }
 }
